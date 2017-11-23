da80371ddc7e2b078022a3cb91403be171e40037
Roman Hotsiy <gotsijroman@gmail.com>
2017-11-23 12:59:28 +0200

    Install ReDoc

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { RedocStandalone } from 'redoc';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <RedocStandalone specUrl="http://rebilly.github.io/ReDoc/swagger.yaml" />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
