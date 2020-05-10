b220b861c7e144be742b0255c12f542264e8f391
Pavel Ryabov <prpbox@gmail.com>
2020-05-10 22:02:28 +0300

    Fix issue with core-js (#16)

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <RedocStandalone specUrl="http://rebilly.github.io/ReDoc/swagger.yaml" />
+        <RedocStandalone specUrl="https://redocly.github.io/redoc/openapi.yaml" />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
